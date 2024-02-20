d1ed5a11e39a3b0972bb7a944727953b4a90c993
Tatyanochka <tanyasmit16@yandex.ru>
2024-02-20 19:47:44 +0300

    initial commit

---
+++ src/JFT_2.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class JFT_2 {
+    public static void main(String[] args) {
+        float accScore = 100;
+        float added = 1205;
+        int splitter = 100;
+        int bonuses = 0;
+        int bonus;
+        if (added >= 1000) {
+            bonus = (int) added / splitter;
+            accScore = accScore + added;
+            bonuses = bonuses + bonus;
+        } else {
+            accScore = accScore + added;
+        }
+        System.out.println("Your scour = " + accScore);
+        System.out.println("Your bonuses = " + bonuses);
+    }
+}
